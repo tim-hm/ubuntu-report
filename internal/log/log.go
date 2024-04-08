056a33b103569656a3addd72d816cc3754a114ba
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2024-04-08 13:32:45 +0100

    wip: Log to file for promtail scraping

--- internal/log/log.go
+++ internal/log/log.go
@@ -8,18 +8,15 @@ import (
 var globalLevel = &slog.LevelVar{}
 
 func init() {
-	logFile, err := os.OpenFile("/var/log/charm_workload.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile("/var/log/ubuntu-reportd.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0755)
 	if err != nil {
 		panic("failed to open log file: " + err.Error())
 	}
 
 	h := slog.NewTextHandler(logFile, &slog.HandlerOptions{Level: globalLevel})
+
 	slog.SetDefault(slog.New(h))
 	globalLevel.Set(slog.LevelWarn)
-
-	//h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: globalLevel})
-	//slog.SetDefault(slog.New(h))
-	//globalLevel.Set(slog.LevelWarn)
 }
 
 // SetLevel change global handler log level.
