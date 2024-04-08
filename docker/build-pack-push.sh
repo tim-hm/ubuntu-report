2bcf4edd4ee64079daf2ebb54468f30fb68ea23d
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2024-04-08 13:31:29 +0100

    ci: Determine oci tag using local git short sha

--- docker/build-pack-push.sh
+++ docker/build-pack-push.sh
@@ -5,8 +5,9 @@ set -uexo pipefail
 echo "This is for local development use only"
 
 REPO=tim-hm
-TAG=sha-9e08649
+SHORT_SHA=$(git rev-parse --short HEAD)
+TAG="sha-${SHORT_SHA}"
 
 go build -o build/ubuntu-reportd ./cmd/ubuntu-reportd
-docker build -f docker/ubuntu-reportd/Dockerfile -t ghcr.io/$REPO/ubuntu-report:$TAG .
-docker push ghcr.io/$REPO/ubuntu-report:$TAG
+docker build -f docker/ubuntu-reportd/Dockerfile -t "ghcr.io/${REPO}/ubuntu-report:${TAG}" .
+docker push "ghcr.io/${REPO}/ubuntu-report:${TAG}"
